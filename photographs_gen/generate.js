68c14414c5634d07ffed682acceb177b5e662cb4
Herpiko Dwi Aguno <herpiko@play3.gg>
2023-04-02 19:35:13 +0700

    Add photograph collection.

---
+++ photographs_gen/generate.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const pkg = require('./package.json')
+const fs = require('fs');
+const async = require('async');
+const directoryPath = path.join(__dirname, '../static/photos');
+console.log('Generate photos...');
+fs.readdir(directoryPath, function(err, files) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  let photos = []
+  async.eachSeries(files, (file, cb) => {
+		console.log(file);
+    if (file.indexOf('DS_Store') > -1) {
+      cb();
+      return;
+    }
+    photos.push({src: '/photos/' + file, height:3, width:4});
+		cb();
+  }, (err) => {
+  	if (err) {
+  	  return console.log('Unable to scan directory: ' + err);
+  	}
+		let output = 'export const photos = ' + JSON.stringify(photos)	
+		fs.writeFileSync(__dirname + '/src/photos.js', output);
+		console.log('Done');
+  })
+});
---
+++ photographs_gen/src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useCallback} from 'react';
+import {render} from 'react-dom';
+import Gallery from 'react-photo-gallery';
+import Carousel, {Modal, ModalGateway} from 'react-images';
+import {photos} from './photos';
+import './App.css';
+
+function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, {photo, index}) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Gallery photos={photos} onClick={openLightbox} targetRowHeight={200} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
+}
+export default App;
